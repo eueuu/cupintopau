39df7311cc435534fd36e308baaf751e3e145354
UNASP-EC - Laboratorio Escola Básica - 01 <lab01eb.ec@unasp.edu.br>
2025-10-31 08:44:58 -0300

    Adiciona array de músicas e explicações

--- anotacoes.js
+++ anotacoes.js
@@ -12,6 +12,35 @@ P: Como conectar o JavaScript com o HTML?
 R: Usando a tag <script> no final do body do HTML:
    <script src="script.js"></script>
 
+P: O que é um array de objetos?
+R: É uma lista que guarda vários "pacotes" de informações relacionadas.
+   Exemplo: cada música tem título, artista e capa.
+
+P: Por que criar dados falsos?
+R: Para testar o site enquanto não temos um banco de dados real.
+   Ajuda a ver como o site vai ficar com dados reais.
+
+P: Como esse array será usado?
+R: JavaScript vai pegar esses dados e criar os cards do Spotify 
+   automaticamente para cada música da lista.
+
 */
 
-// Código JavaScript será adicionado aqui quando necessário
+// Array de músicas (como se fosse um banco de dados)
+const musicas = [
+    {
+        titulo: "Bohemian Rhapsody",
+        artista: "Queen",
+        capaUrl: "imagens/bohemian.jpg"
+    },
+    {
+        titulo: "Billie Jean",
+        artista: "Michael Jackson",
+        capaUrl: "imagens/billie-jean.jpg"
+    },
+    {
+        titulo: "Garota de Ipanema",
+        artista: "Tom Jobim",
+        capaUrl: "imagens/ipanema.jpg"
+    }
+];
---
+++ app.js
@@ -0,0 +1,18 @@
+// Array de músicas (como se fosse um banco de dados)
+const musicas = [
+    {
+        titulo: "Bohemian Rhapsody",
+        artista: "Queen",
+        capaUrl: "imagens/bohemian.jpg"
+    },
+    {
+        titulo: "Billie Jean",
+        artista: "Michael Jackson",
+        capaUrl: "imagens/billie-jean.jpg"
+    },
+    {
+        titulo: "Garota de Ipanema",
+        artista: "Tom Jobim",
+        capaUrl: "imagens/ipanema.jpg"
+    }
+];
