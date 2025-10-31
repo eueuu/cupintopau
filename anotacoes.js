7f3469719f26a117d64fd3a2ecb5e6560dfb59b2
UNASP-EC - Laboratorio Escola Básica - 01 <lab01eb.ec@unasp.edu.br>
2025-10-31 08:42:15 -0300

    Adiciona arquivos de anotações HTML e JS

---
+++ anotacoes.js
@@ -0,0 +1,17 @@
+/* DÚVIDAS E RESPOSTAS SOBRE O CÓDIGO JAVASCRIPT
+
+P: Quando precisamos usar JavaScript ao invés de CSS?
+R: Para interações mais complexas como:
+   - Tocar música
+   - Abrir menus
+   - Animações complexas
+   - Comunicação com servidor
+   - Mudanças que dependem de dados ou cálculos
+
+P: Como conectar o JavaScript com o HTML?
+R: Usando a tag <script> no final do body do HTML:
+   <script src="script.js"></script>
+
+*/
+
+// Código JavaScript será adicionado aqui quando necessário
